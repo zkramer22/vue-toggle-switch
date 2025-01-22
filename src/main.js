aa34b9921ee698b3fafa67f584d7fb38f9a8fae8
zach kramer <zkramer22@gmail.com>
2025-01-21 17:30:32 -0800

    ready for build

--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
-import { createApp } from 'vue';
-import App from './App.vue'; // Your test App
-
-console.log('Mounting app...');
-
-const app = createApp(App);
-app.mount('#app');
+import VueToggleSwitch from './VueToggleSwitch.vue'
+export default VueToggleSwitch
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 export default defineConfig({
     plugins: [vue()],
     build: {
         lib: {
-            // entry: 'src/VueToggleSwitch.vue', // Path to your component
             entry: path.resolve(__dirname, 'src/main.js'),  // Entry point for the component
             name: 'VueToggleSwitch', // Name of your library
             fileName: (format) => `vue-toggle-switch.${format}.js`, // Output file format
