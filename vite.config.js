9919c95792f4de7ca5239b38f8f475b20ccff033
zach kramer <zkramer22@gmail.com>
2025-01-22 20:21:28 -0800

    update build configs

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,61 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
-    const isDev = mode === 'development';
+    const isDev = mode === 'development'
+    const isApp = mode === 'app'
+    const outDir = isApp ? 'gh-pages-dist' : 'dist'
 
     return {
         plugins: [vue()],
         root: isDev ? 'dev' : '', // Use 'dev/' as the root directory for development
         build: {
-            outDir: 'dist',
-            lib: {
-                entry: path.resolve(__dirname, 'src/main.js'), // Entry point for the library build
-                name: 'VueToggleSwitch',
-                fileName: 'vue-toggle-switch', // Base name for the files
-            },
-            rollupOptions: {
-                external: ['vue'], // Do not bundle Vue
-                output: [
-                    {
-                        format: 'umd', // UMD format for general compatibility
-                        dir: 'dist', // Directory for the output files
-                        entryFileNames: 'vue-toggle-switch.umd.js',
-                        name: 'VueToggleSwitch',
-                        globals: {
-                            vue: 'Vue',
+            outDir,
+            lib: !isApp 
+                ? {
+                    entry: path.resolve(__dirname, 'src/main.js'), // Entry point for the library build
+                    name: 'VueToggleSwitch',
+                    fileName: 'vue-toggle-switch', // Base name for the files
+                } 
+                : undefined,
+            rollupOptions: !isApp 
+                ? {
+                    external: ['vue'], // Do not bundle Vue
+                    output: [
+                        {
+                            format: 'umd', // UMD format for general compatibility
+                            dir: outDir, // Directory for the output files
+                            entryFileNames: 'vue-toggle-switch.umd.js',
+                            name: 'VueToggleSwitch',
+                            globals: {
+                                vue: 'Vue',
+                            },
                         },
-                    },
-                    {
-                        format: 'esm', // ESM format for modern JavaScript users
-                        dir: 'dist',
-                        entryFileNames: 'vue-toggle-switch.esm.js',
-                        globals: {
-                            vue: 'Vue',
+                        {
+                            format: 'esm', // ESM format for modern JavaScript users
+                            dir: outDir,
+                            entryFileNames: 'vue-toggle-switch.esm.js',
+                            globals: {
+                                vue: 'Vue',
+                            },
                         },
-                    },
-                    {
-                        format: 'cjs', // CommonJS format for Node.js users
-                        dir: 'dist',
-                        entryFileNames: 'vue-toggle-switch.cjs.js',
-                        globals: {
-                            vue: 'Vue',
+                        {
+                            format: 'cjs', // CommonJS format for Node.js users
+                            dir: outDir,
+                            entryFileNames: 'vue-toggle-switch.cjs.js',
+                            globals: {
+                                vue: 'Vue',
+                            },
                         },
-                    },
-                ],
-            },
+                    ],
+                }
+                : undefined,
         },
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, isDev ? 'dev' : 'src'), // Use 'dev' aliases for development
             },
         },
-    };
-});
+    }
+})
